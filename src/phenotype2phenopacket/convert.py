152f60b6c77f8071a22df3e7b39c7f856573e86e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-19 16:27:15 +0100

    tox lint

--- src/phenotype2phenopacket/convert.py
+++ src/phenotype2phenopacket/convert.py
@@ -1,16 +1,22 @@
-from pathlib import Path
 import re
+from pathlib import Path
 
 import click
 import polars as pl
 from google.protobuf.json_format import MessageToJson
-from phenopackets import (
-    Resource, MetaData, Individual, PhenotypicFeature, OntologyClass, Disease, Phenopacket,
-    TimeElement
-)
+from google.protobuf.timestamp_pb2 import Timestamp
 from oaklib.implementations.pronto.pronto_implementation import ProntoImplementation
 from oaklib.resource import OntologyResource
-from google.protobuf.timestamp_pb2 import Timestamp
+from phenopackets import (
+    Disease,
+    Individual,
+    MetaData,
+    OntologyClass,
+    Phenopacket,
+    PhenotypicFeature,
+    Resource,
+    TimeElement,
+)
 
 
 def load_ontology():
@@ -33,7 +39,7 @@ def write_phenopacket(phenopacket: Phenopacket, output_file: Path) -> None:
 
 
 def create_phenopacket_file_name_from_disease(disease_name: str) -> Path:
-    normalised_string = re.sub(r'\W+', '_', disease_name)
+    normalised_string = re.sub(r"\W+", "_", disease_name)
     return Path(normalised_string.replace(" ", "_") + ".json")
 
 
@@ -50,7 +56,9 @@ def create_onset(human_phenotype_ontology, phenotype_annotation_entry: dict) -> 
     if phenotype_annotation_entry["onset"] is not None:
         rels = human_phenotype_ontology.entity_alias_map(phenotype_annotation_entry["onset"])
         term = "".join(rels[(list(rels.keys())[0])])
-        return TimeElement(ontology_class=OntologyClass(id=phenotype_annotation_entry["onset"], label=term))
+        return TimeElement(
+            ontology_class=OntologyClass(id=phenotype_annotation_entry["onset"], label=term)
+        )
     else:
         return None
 
@@ -64,20 +72,28 @@ def create_modifier(human_phenotype_ontology, phenotype_annotation_entry: dict) 
         return None
 
 
-def create_phenotypic_feature(human_phenotype_ontology, phenotype_annotation_entry: dict) -> PhenotypicFeature:
+def create_phenotypic_feature(
+    human_phenotype_ontology, phenotype_annotation_entry: dict
+) -> PhenotypicFeature:
     if phenotype_annotation_entry["aspect"] == "P":
         rels = human_phenotype_ontology.entity_alias_map(phenotype_annotation_entry["hpo_id"])
         hpo_term = "".join(rels[(list(rels.keys())[0])])
-        return PhenotypicFeature(type=OntologyClass(id=phenotype_annotation_entry["hpo_id"], label=hpo_term),
-                                 onset=create_onset(human_phenotype_ontology, phenotype_annotation_entry),
-                                 modifiers=create_modifier(human_phenotype_ontology, phenotype_annotation_entry))
+        return PhenotypicFeature(
+            type=OntologyClass(id=phenotype_annotation_entry["hpo_id"], label=hpo_term),
+            onset=create_onset(human_phenotype_ontology, phenotype_annotation_entry),
+            modifiers=create_modifier(human_phenotype_ontology, phenotype_annotation_entry),
+        )
     else:
         return None
 
 
 def create_disease(phenotype_annotation_entry: dict) -> Disease:
-    return Disease(term=OntologyClass(id=phenotype_annotation_entry["database_id"],
-                                      label=phenotype_annotation_entry["disease_name"]))
+    return Disease(
+        term=OntologyClass(
+            id=phenotype_annotation_entry["database_id"],
+            label=phenotype_annotation_entry["disease_name"],
+        )
+    )
 
 
 def create_phenotype_ontology_resource() -> Resource:
@@ -94,22 +110,26 @@ def create_phenotype_ontology_resource() -> Resource:
 def create_metadata() -> MetaData:
     timestamp = Timestamp()
     timestamp.GetCurrentTime()
-    return MetaData(created=timestamp,
-                    created_by="phenotype2phenopacket",
-                    resources=[create_phenotype_ontology_resource()],
-                    phenopacket_schema_version="2.0")
+    return MetaData(
+        created=timestamp,
+        created_by="phenotype2phenopacket",
+        resources=[create_phenotype_ontology_resource()],
+        phenopacket_schema_version="2.0",
+    )
 
 
 def convert_to_phenopackets(phenotype_annotation: pl.DataFrame, output_dir: Path):
     human_phenotype_ontology = load_ontology()
     omim_diseases = phenotype_annotation.filter(pl.col("database_id").str.starts_with("OMIM"))
-    grouped_omim_diseases = omim_diseases.partition_by(by='database_id', maintain_order=True)
+    grouped_omim_diseases = omim_diseases.partition_by(by="database_id", maintain_order=True)
     for omim_disease in grouped_omim_diseases:
         phenotypic_features = []
         phenotype_entry = ""
         for phenotype in omim_disease.rows(named=True):
-            phenotypic_feature = create_phenotypic_feature(human_phenotype_ontology=human_phenotype_ontology,
-                                                           phenotype_annotation_entry=phenotype)
+            phenotypic_feature = create_phenotypic_feature(
+                human_phenotype_ontology=human_phenotype_ontology,
+                phenotype_annotation_entry=phenotype,
+            )
             if phenotypic_feature is not None:
                 phenotypic_features.append(phenotypic_feature)
             else:
@@ -122,8 +142,12 @@ def convert_to_phenopackets(phenotype_annotation: pl.DataFrame, output_dir: Path
             diseases=[create_disease(phenotype_entry)],
             meta_data=create_metadata(),
         )
-        write_phenopacket(phenopacket, output_dir.joinpath(
-            create_phenopacket_file_name_from_disease(phenotype_entry["disease_name"])))
+        write_phenopacket(
+            phenopacket,
+            output_dir.joinpath(
+                create_phenopacket_file_name_from_disease(phenotype_entry["disease_name"])
+            ),
+        )
 
 
 @click.command("convert")
