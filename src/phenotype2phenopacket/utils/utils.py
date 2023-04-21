0d42a9470c8087fbcb730eba024f431074d02198
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-21 19:07:38 +0100

    adding file utility methods

--- src/phenotype2phenopacket/utils/utils.py
+++ src/phenotype2phenopacket/utils/utils.py
@@ -30,3 +30,14 @@ def read_hgnc_data(hgnc_data_file: Path) -> pd.DataFrame:
 def read_phenotype_annotation_file(phenotype_annotation_file_path: Path) -> pl.DataFrame:
     """Read a phenotype annotation file and return a polars dataframe"""
     return pl.read_csv(phenotype_annotation_file_path, separator="\t", comment_char="#")
+
+
+def all_files(directory: Path) -> list[Path]:
+    """Obtains all files from a given directory."""
+    files = [path for path in directory.iterdir()]
+    files.sort()
+    return files
+
+
+def read_variant_summary(variant_summary: Path) -> pl.DataFrame:
+    return pl.read_csv(variant_summary, separator="\t", infer_schema_length=10000)
