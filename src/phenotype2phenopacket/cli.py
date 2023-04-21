3ca37d3247dcd25c1a2dd3c2a6f3390ca09b7cac
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-21 16:15:12 +0100

    add add_variants_command

--- src/phenotype2phenopacket/cli.py
+++ src/phenotype2phenopacket/cli.py
@@ -1,5 +1,6 @@
 import click
 
+from phenotype2phenopacket.add import add_variants_command
 from phenotype2phenopacket.convert import convert_to_phenopackets_command
 
 
@@ -9,6 +10,6 @@ def main():
 
 
 main.add_command(convert_to_phenopackets_command)
-
+main.add_command(add_variants_command)
 if __name__ == "__main__":
     main()
