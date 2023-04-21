6e104a8167ac1d5008620abaa4362e7d946bb497
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-21 17:39:06 +0100

    refactoring imports

--- src/phenotype2phenopacket/cli.py
+++ src/phenotype2phenopacket/cli.py
@@ -1,7 +1,7 @@
 import click
 
-from phenotype2phenopacket.add import add_variants_command
-from phenotype2phenopacket.convert import convert_to_phenopackets_command
+# from phenotype2phenopacket.add import add_variants_command
+from phenotype2phenopacket.cli_convert import convert_to_phenopackets_command
 
 
 @click.group()
@@ -10,6 +10,6 @@ def main():
 
 
 main.add_command(convert_to_phenopackets_command)
-main.add_command(add_variants_command)
+# main.add_command(add_variants_command)
 if __name__ == "__main__":
     main()
