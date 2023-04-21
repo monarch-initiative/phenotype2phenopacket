67436e701fbe742349727fbd3315a13b83fbc7a2
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-21 17:57:26 +0100

    creating VariantSummaryFilter for filtering variant summary data

--- src/phenotype2phenopacket/add/add_variants.py
+++ src/phenotype2phenopacket/add/add_variants.py
@@ -20,25 +20,65 @@ from phenopackets import (
     VcfRecord,
 )
 
-from phenotype2phenopacket.convert import write_phenopacket
+from phenotype2phenopacket.utils.phenopacket_utils import write_phenopacket
+from phenotype2phenopacket.utils.utils import all_files, read_variant_summary
 
 
-def all_files(directory: Path) -> list[Path]:
-    """Obtains all files from a given directory."""
-    files = [path for path in directory.iterdir()]
-    files.sort()
-    return files
+class VariantSummaryFilter:
+    def __init__(
+            self,
+            variant_summary_df: pl.DataFrame,
+            disease: Disease,
+            clinical_significance_filter: int,
+            genome_assembly_filter: str,
+    ):
+        self.variant_summary_df = variant_summary_df
+        self.disease = disease
+        self.clinical_significance_filter = clinical_significance_filter
+        self.genome_assembly_filter = genome_assembly_filter
 
+    def filter_for_disease(self) -> pl.DataFrame:
+        return self.variant_summary_df.filter(
+            pl.col("PhenotypeIDS").str.contains(r"\b" + re.escape(self.disease.term.id) + r"\b")
+        )
 
-def read_variant_summary(variant_summary: Path) -> pl.DataFrame:
-    return pl.read_csv(variant_summary, separator="\t", infer_schema_length=10000)
+    @staticmethod
+    def map_clinical_significance(clinical_significance) -> int:
+        mapping = {
+            "Pathogenic": ClinicalSignificance.PATHOGENIC.value,
+            "Likely pathogenic": ClinicalSignificance.LIKELY_PATHOGENIC.value,
+            "Uncertain significance": ClinicalSignificance.UNCERTAIN_SIGNIFICANCE.value,
+            "Likely benign": ClinicalSignificance.LIKELY_BENIGN.value,
+            "Benign": ClinicalSignificance.BENIGN.value,
+            "Not provided": ClinicalSignificance.NOT_PROVIDED.value,
+        }
+        return mapping.get(clinical_significance, None)
 
+    def filter_for_clinical_significance(self, filtered_disease_variant_summary):
+        mapped_clinical_significance = filtered_disease_variant_summary.select(
+            [
+                pl.col("ClinicalSignificance").apply(lambda x: self.map_clinical_significance(x)),
+                pl.exclude("ClinicalSignificance"),
+            ],
+            skip_nulls=True,
+        )
 
-def filter_for_disease(variant_summary_df: pl.DataFrame, disease: Disease) -> pl.DataFrame:
-    print(disease.term.id)
-    return variant_summary_df.filter(
-        pl.col("PhenotypeIDS").str.contains(r"\b" + re.escape(disease.term.id) + r"\b")
-    )
+        return mapped_clinical_significance.filter(
+            pl.col("ClinicalSignificance") >= self.clinical_significance_filter
+        )
+
+    def filter_for_genome_assembly(self, filtered_clinical_significance_variant_summary):
+        return filtered_clinical_significance_variant_summary.filter(
+            pl.col("Assembly") == self.genome_assembly_filter
+        )
+
+    def filter_variant_summary(self):
+        disease_variant_summary = self.filter_for_disease()
+        if len(disease_variant_summary) == 0:
+            print("No disease matches")
+            return None
+        filtered_variants = self.filter_for_clinical_significance(disease_variant_summary)
+        return self.filter_for_genome_assembly(filtered_variants)
 
 
 class ClinicalSignificance(Enum):
@@ -48,53 +88,6 @@ class ClinicalSignificance(Enum):
     LIKELY_BENIGN = 2
     BENIGN = 1
     NOT_PROVIDED = 0
-
-
-def map_clinical_significance(clinical_significance) -> int:
-    mapping = {
-        "Pathogenic": ClinicalSignificance.PATHOGENIC.value,
-        "Likely pathogenic": ClinicalSignificance.LIKELY_PATHOGENIC.value,
-        "Uncertain significance": ClinicalSignificance.UNCERTAIN_SIGNIFICANCE.value,
-        "Likely benign": ClinicalSignificance.LIKELY_BENIGN.value,
-        "Benign": ClinicalSignificance.BENIGN.value,
-        "Not provided": ClinicalSignificance.NOT_PROVIDED.value,
-    }
-    return mapping.get(clinical_significance, None)
-
-
-def filter_for_clinical_significance(
-    variant_summary_df: pl.DataFrame, clinical_significance_filter: int
-):
-    mapped_clinical_significance = variant_summary_df.select(
-        [
-            pl.col("ClinicalSignificance").apply(lambda x: map_clinical_significance(x)),
-            pl.exclude("ClinicalSignificance"),
-        ],
-        skip_nulls=True,
-    )
-
-    return mapped_clinical_significance.filter(
-        pl.col("ClinicalSignificance") >= clinical_significance_filter
-    )
-
-
-def filter_for_genome_assembly(variant_summary_df, genome_assembly_filter):
-    return variant_summary_df.filter(pl.col("Assembly") == genome_assembly_filter)
-
-
-def filter_variant_summary(
-    variant_summary_df, disease, clinical_significance_filter, genome_assembly_filter
-):
-    disease_variant_summary = filter_for_disease(
-        variant_summary_df=variant_summary_df, disease=disease
-    )
-    if len(disease_variant_summary) == 0:
-        print("No disease matches")
-        return None
-    filtered_variants = filter_for_clinical_significance(
-        disease_variant_summary, clinical_significance_filter
-    )
-    return filter_for_genome_assembly(filtered_variants, genome_assembly_filter)
 
 
 def return_phenopacket_disease(phenopacket: Phenopacket) -> [str]:
@@ -129,14 +122,17 @@ def add_variant_genomic_interpretation(variant_entry: dict) -> GenomicInterpreta
 
 
 def add_variant_interpretations(
-    disease: Disease,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
+        disease: Disease,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
 ):
-    filtered_variant_summary = filter_variant_summary(
-        variant_summary_df, disease, clinical_significance_filter, genome_assembly_filter
-    )
+    filtered_variant_summary = VariantSummaryFilter(
+        variant_summary_df=variant_summary_df,
+        disease=disease,
+        clinical_significance_filter=clinical_significance_filter,
+        genome_assembly_filter=genome_assembly_filter,
+    ).filter_variant_summary()
     if filtered_variant_summary is None:
         return None
     genomic_interpretations = []
@@ -146,10 +142,10 @@ def add_variant_interpretations(
 
 
 def add_diagnosis(
-    disease: Disease,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
+        disease: Disease,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
 ) -> Diagnosis:
     genomic_interpretations = add_variant_interpretations(
         disease, variant_summary_df, clinical_significance_filter, genome_assembly_filter
@@ -166,10 +162,10 @@ def add_diagnosis(
 
 
 def create_interpretation(
-    phenopacket: Phenopacket,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
+        phenopacket: Phenopacket,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
 ) -> Interpretation:
     disease = return_phenopacket_disease(phenopacket)
     diagnosis = add_diagnosis(
@@ -196,10 +192,10 @@ def phenopacket_reader(file: Path):
 
 
 def add_variant_interpretations_to_phenopacket(
-    phenopacket: Phenopacket,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
+        phenopacket: Phenopacket,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
 ):
     interpretation = create_interpretation(
         phenopacket, variant_summary_df, clinical_significance_filter, genome_assembly_filter
@@ -212,27 +208,26 @@ def add_variant_interpretations_to_phenopacket(
 
 
 def add_variants(
-    phenopacket_path: Path,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
-    output_dir: Path,
+        phenopacket_path: Path,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
+        output_dir: Path,
 ):
     phenopacket = phenopacket_reader(phenopacket_path)
     phenopacket_with_variants = add_variant_interpretations_to_phenopacket(
         phenopacket, variant_summary_df, clinical_significance_filter, genome_assembly_filter
     )
     if phenopacket_with_variants is not None:
-        print("writing")
         write_phenopacket(phenopacket_with_variants, output_dir.joinpath(phenopacket_path.name))
 
 
 def add_variants_to_directory(
-    phenopacket_dir: Path,
-    variant_summary_df: pl.DataFrame,
-    clinical_significance_filter: int,
-    genome_assembly_filter: str,
-    output_dir: Path,
+        phenopacket_dir: Path,
+        variant_summary_df: pl.DataFrame,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
+        output_dir: Path,
 ):
     for file in all_files(phenopacket_dir):
         add_variants(
@@ -264,7 +259,7 @@ def add_variants_to_directory(
     "-c",
     required=True,
     help="Clinical significance filter for variants. 0:NOT_PROVIDED, 1:BENIGN, 2:LIKELY_BENIGN, "
-    "3:UNCERTAIN_SIGNIFICANCE, 4:LIKELY_PATHOGENIC, 5:PATHOGENIC ",
+         "3:UNCERTAIN_SIGNIFICANCE, 4:LIKELY_PATHOGENIC, 5:PATHOGENIC ",
     type=click.Choice(["0", "1", "2", "3", "4", "5"], case_sensitive=False),
 )
 @click.option(
@@ -282,11 +277,11 @@ def add_variants_to_directory(
     type=Path,
 )
 def add_variants_command(
-    phenopacket_dir: Path,
-    variant_summary: Path,
-    clinical_significance_filter: str,
-    genome_assembly_filter: str,
-    output_dir: Path,
+        phenopacket_dir: Path,
+        variant_summary: Path,
+        clinical_significance_filter: str,
+        genome_assembly_filter: str,
+        output_dir: Path,
 ):
     output_dir.mkdir(exist_ok=True)
     variant_summary_df = read_variant_summary(variant_summary)
