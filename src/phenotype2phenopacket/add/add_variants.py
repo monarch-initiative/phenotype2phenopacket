3aea1857dc9112c2ce633f0da9a7d084be03adc7
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-04-21 19:06:30 +0100

    tox lint

--- src/phenotype2phenopacket/add/add_variants.py
+++ src/phenotype2phenopacket/add/add_variants.py
@@ -1,27 +1,17 @@
-import json
 import re
 from enum import Enum
 from pathlib import Path
 
-import click
 import polars as pl
-from google.protobuf.json_format import Parse
-from phenopackets import (
-    Diagnosis,
-    Disease,
-    Family,
-    GeneDescriptor,
-    GenomicInterpretation,
-    Interpretation,
-    OntologyClass,
-    Phenopacket,
-    VariantInterpretation,
-    VariationDescriptor,
-    VcfRecord,
-)
+from phenopackets import Disease
 
-from phenotype2phenopacket.utils.phenopacket_utils import write_phenopacket
-from phenotype2phenopacket.utils.utils import all_files, read_variant_summary
+from phenotype2phenopacket.utils.phenopacket_utils import (
+    PhenopacketInterpretationExtender,
+    PhenopacketUtil,
+    phenopacket_reader,
+    write_phenopacket,
+)
+from phenotype2phenopacket.utils.utils import all_files
 
 
 class ClinicalSignificance(Enum):
@@ -35,11 +25,11 @@ class ClinicalSignificance(Enum):
 
 class VariantSummaryFilter:
     def __init__(
-            self,
-            variant_summary_df: pl.DataFrame,
-            disease: Disease,
-            clinical_significance_filter: int,
-            genome_assembly_filter: str,
+        self,
+        variant_summary_df: pl.DataFrame,
+        disease: Disease,
+        clinical_significance_filter: int,
+        genome_assembly_filter: str,
     ):
         self.variant_summary_df = variant_summary_df
         self.disease = disease
@@ -84,49 +74,20 @@ class VariantSummaryFilter:
     def filter_variant_summary(self):
         disease_variant_summary = self.filter_for_disease()
         if len(disease_variant_summary) == 0:
-            print("No disease matches")
             return None
         filtered_variants = self.filter_for_clinical_significance(disease_variant_summary)
         return self.filter_for_genome_assembly(filtered_variants)
 
 
-def return_phenopacket_disease(phenopacket: Phenopacket) -> [str]:
-    return phenopacket.diseases[0]
-
-
-def add_variant_genomic_interpretation(variant_entry: dict) -> GenomicInterpretation:
-    return GenomicInterpretation(
-        subject_or_biosample_id="patient1",
-        interpretation_status=0,
-        variant_interpretation=VariantInterpretation(
-            acmg_pathogenicity_classification=variant_entry["ClinicalSignificance"],
-            variation_descriptor=VariationDescriptor(
-                id="clinvar:" + str(variant_entry["VariationID"]),
-                gene_context=GeneDescriptor(
-                    value_id=variant_entry["HGNC_ID"], symbol=variant_entry["GeneSymbol"]
-                ),
-                vcf_record=VcfRecord(
-                    genome_assembly=variant_entry["Assembly"],
-                    chrom=variant_entry["Chromosome"],
-                    pos=variant_entry["Start"],
-                    ref=variant_entry["ReferenceAllele"]
-                    if variant_entry["ReferenceAllele"] != "na"
-                    else variant_entry["ReferenceAlleleVCF"],
-                    alt=variant_entry["AlternateAllele"]
-                    if variant_entry["AlternateAllele"] != "na"
-                    else variant_entry["AlternateAlleleVCF"],
-                ),
-            ),
-        ),
-    )
-
-
-def add_variant_interpretations(
-        disease: Disease,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
+def add_variants(
+    phenopacket_path: Path,
+    variant_summary_df: pl.DataFrame,
+    clinical_significance_filter: int,
+    genome_assembly_filter: str,
+    output_dir: Path,
 ):
+    phenopacket = phenopacket_reader(phenopacket_path)
+    disease = PhenopacketUtil(phenopacket).return_phenopacket_disease()
     filtered_variant_summary = VariantSummaryFilter(
         variant_summary_df=variant_summary_df,
         disease=disease,
@@ -134,100 +95,20 @@ def add_variant_interpretations(
         genome_assembly_filter=genome_assembly_filter,
     ).filter_variant_summary()
     if filtered_variant_summary is None:
-        return None
-    genomic_interpretations = []
-    for row in filtered_variant_summary.rows(named=True):
-        genomic_interpretations.append(add_variant_genomic_interpretation(row))
-    return genomic_interpretations
-
-
-def add_diagnosis(
-        disease: Disease,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
-) -> Diagnosis:
-    genomic_interpretations = add_variant_interpretations(
-        disease, variant_summary_df, clinical_significance_filter, genome_assembly_filter
-    )
-    if genomic_interpretations is None:
-        return None
-    return Diagnosis(
-        disease=OntologyClass(
-            id=disease.term.id,
-            label=disease.term.label,
-        ),
-        genomic_interpretations=genomic_interpretations,
-    )
-
-
-def create_interpretation(
-        phenopacket: Phenopacket,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
-) -> Interpretation:
-    disease = return_phenopacket_disease(phenopacket)
-    diagnosis = add_diagnosis(
-        disease, variant_summary_df, clinical_significance_filter, genome_assembly_filter
-    )
-    if diagnosis is None:
-        return None
-    return Interpretation(
-        id=disease.term.label + "-interpretation",
-        progress_status=0,
-        diagnosis=diagnosis,
-    )
-
-
-def phenopacket_reader(file: Path):
-    """Reads a phenopacket file, returning its contents."""
-    file = open(file, "r")
-    phenopacket = json.load(file)
-    file.close()
-    if "proband" in phenopacket:
-        return Parse(json.dumps(phenopacket), Family())
+        print("No disease matches")
     else:
-        return Parse(json.dumps(phenopacket), Phenopacket())
-
-
-def add_variant_interpretations_to_phenopacket(
-        phenopacket: Phenopacket,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
-):
-    interpretation = create_interpretation(
-        phenopacket, variant_summary_df, clinical_significance_filter, genome_assembly_filter
-    )
-    if interpretation is not None:
-        phenopacket.interpretations.extend([interpretation])
-        return phenopacket
-    else:
-        return None
-
-
-def add_variants(
-        phenopacket_path: Path,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
-        output_dir: Path,
-):
-    phenopacket = phenopacket_reader(phenopacket_path)
-    phenopacket_with_variants = add_variant_interpretations_to_phenopacket(
-        phenopacket, variant_summary_df, clinical_significance_filter, genome_assembly_filter
-    )
-    if phenopacket_with_variants is not None:
+        phenopacket_with_variants = PhenopacketInterpretationExtender(
+            phenopacket
+        ).add_variant_interpretation_to_phenopacket(filtered_variant_summary)
         write_phenopacket(phenopacket_with_variants, output_dir.joinpath(phenopacket_path.name))
 
 
 def add_variants_to_directory(
-        phenopacket_dir: Path,
-        variant_summary_df: pl.DataFrame,
-        clinical_significance_filter: int,
-        genome_assembly_filter: str,
-        output_dir: Path,
+    phenopacket_dir: Path,
+    variant_summary_df: pl.DataFrame,
+    clinical_significance_filter: int,
+    genome_assembly_filter: str,
+    output_dir: Path,
 ):
     for file in all_files(phenopacket_dir):
         add_variants(
@@ -237,58 +118,3 @@ def add_variants_to_directory(
             genome_assembly_filter,
             output_dir,
         )
-
-
-@click.command("add-variants")
-@click.option(
-    "--phenopacket-dir",
-    "-p",
-    required=True,
-    help="Path to phenopacket directory.",
-    type=Path,
-)
-@click.option(
-    "--variant-summary",
-    "-v",
-    required=True,
-    help="Path to variant summary data file.",
-    type=Path,
-)
-@click.option(
-    "--clinical-significance-filter",
-    "-c",
-    required=True,
-    help="Clinical significance filter for variants. 0:NOT_PROVIDED, 1:BENIGN, 2:LIKELY_BENIGN, "
-         "3:UNCERTAIN_SIGNIFICANCE, 4:LIKELY_PATHOGENIC, 5:PATHOGENIC ",
-    type=click.Choice(["0", "1", "2", "3", "4", "5"], case_sensitive=False),
-)
-@click.option(
-    "--genome-assembly-filter",
-    "-g",
-    required=True,
-    help="Genome assembly filter",
-    type=click.Choice(["GRCh37", "GRCh38"], case_sensitive=True),
-)
-@click.option(
-    "--output-dir",
-    "-o",
-    required=True,
-    help="Path to output directory.",
-    type=Path,
-)
-def add_variants_command(
-        phenopacket_dir: Path,
-        variant_summary: Path,
-        clinical_significance_filter: str,
-        genome_assembly_filter: str,
-        output_dir: Path,
-):
-    output_dir.mkdir(exist_ok=True)
-    variant_summary_df = read_variant_summary(variant_summary)
-    add_variants_to_directory(
-        phenopacket_dir,
-        variant_summary_df,
-        int(clinical_significance_filter),
-        genome_assembly_filter,
-        output_dir,
-    )
