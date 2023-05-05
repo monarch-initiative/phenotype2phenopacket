afae134f036a0c4869319a431b323d73032f32dd
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-05 15:56:21 +0100

    adding num_disease cli parameter

--- src/phenotype2phenopacket/convert/convert.py
+++ src/phenotype2phenopacket/convert/convert.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 
 import polars as pl
@@ -11,12 +12,15 @@ from phenotype2phenopacket.utils.utils import load_ontology
 
 def convert_to_phenopackets(
     phenotype_annotation: pl.DataFrame,
+    num_disease: int,
     output_dir: Path,
 ):
     """Convert phenotype annotation file to a set of disease phenopackets."""
     human_phenotype_ontology = load_ontology()
     omim_diseases = phenotype_annotation.filter(pl.col("database_id").str.starts_with("OMIM"))
     grouped_omim_diseases = omim_diseases.partition_by(by="database_id", maintain_order=True)
+    if num_disease != 0:
+        grouped_omim_diseases = random.sample(grouped_omim_diseases, num_disease)
     for omim_disease in grouped_omim_diseases:
         phenopacket_file = PhenotypeAnnotationToPhenopacketConverter(
             human_phenotype_ontology
