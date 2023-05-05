247ea5f5a5631a19c5201250f1b5b3b343063767
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-05 15:26:22 +0100

    adding methods for creating a synthetic patient

--- src/phenotype2phenopacket/utils/phenopacket_utils.py
+++ src/phenotype2phenopacket/utils/phenopacket_utils.py
@@ -3,6 +3,7 @@ import re
 import secrets
 from copy import copy
 from dataclasses import dataclass
+from fractions import Fraction
 from pathlib import Path
 from typing import Union
 
@@ -24,12 +25,10 @@ from phenopackets import (
     PhenotypicFeature,
     Resource,
     TimeElement,
-    VariantInterpretation,
-    VariationDescriptor,
-    VcfRecord,
 )
 
 from phenotype2phenopacket.utils.gene_map_utils import GeneIdentifierUpdater
+from phenotype2phenopacket.utils.utils import is_float
 
 
 @dataclass
@@ -97,6 +96,192 @@ def write_phenopacket(phenopacket: Phenopacket, output_file: Path) -> None:
     outfile.close()
 
 
+class SyntheticPatientGenerator:
+    def __init__(self, disease_df: pl.DataFrame, ontology):
+        self.disease_df = disease_df
+        self.ontology = ontology
+        self.lower_age = 0
+        self.upper_age = 0
+        self.filtered_df = []
+        self.secret_rand = secrets.SystemRandom()
+
+    def get_number_of_terms(self):
+        """Get number of terms to ascribe from full set."""
+        if len(self.disease_df) == 1:
+            return 1
+        return int(len(self.disease_df) * (self.secret_rand.uniform(0.2, 0.75)))
+
+    @staticmethod
+    def shuffle_dataframe(disease_df: pl.DataFrame):
+        """Shuffle dataframe."""
+        return disease_df.sample(fraction=1, shuffle=True)
+
+    def add_frequency(self):
+        """Add random frequency to annotations without one defined."""
+        updated_df = []
+        for row in self.disease_df.rows(named=True):
+            if row["frequency"] is None:
+                synthetic_frequency = self.secret_rand.uniform(0, 1)
+                row["frequency"] = synthetic_frequency
+                updated_df.append(row)
+            elif row["frequency"] is not None:
+                updated_df.append(row)
+        return pl.from_dicts(updated_df)
+
+    def get_onset_range(self):
+        """Get the onset range from a set of annotations for a disease."""
+        for phenotype_entry in self.disease_df.rows(named=True):
+            if phenotype_entry["onset"] is not None:
+                if self.lower_age < float(onset_hpo[phenotype_entry["onset"]].lower_age):
+                    self.lower_age = float(onset_hpo[phenotype_entry["onset"]].lower_age)
+                if self.upper_age < float(onset_hpo[phenotype_entry["onset"]].upper_age):
+                    self.upper_age = float(onset_hpo[phenotype_entry["onset"]].upper_age)
+        return OnsetTerm(lower_age=self.lower_age, upper_age=self.upper_age)
+
+    def check_hpo_frequency(self, phenotype_entry: dict):
+        """Filter with HPO defined frequencies."""
+        frequency_limits = frequency_hpo[phenotype_entry["frequency"]]
+        random_frequency = self.secret_rand.uniform(0, 100)
+        if (
+            float(frequency_limits.lower) < random_frequency < float(frequency_limits.upper)
+            and phenotype_entry not in self.filtered_df
+        ):
+            self.filtered_df.append(phenotype_entry)
+
+    def check_frequency_threshold(
+        self, frequency: float, phenotype_entry: dict, random_frequency: float
+    ):
+        """Check if patient frequency meets the filter for the disease frequency."""
+        if random_frequency <= float(frequency) and phenotype_entry not in self.filtered_df:
+            self.filtered_df.append(phenotype_entry)
+        else:
+            pass
+
+    def check_percentage_frequency(self, phenotype_entry: dict):
+        """Filter with percentage frequency."""
+        frequency = phenotype_entry["frequency"].strip("%")
+        random_frequency = self.secret_rand.uniform(0, 100)
+        self.check_frequency_threshold(frequency, phenotype_entry, random_frequency)
+
+    def check_fraction_frequency(self, phenotype_entry: dict):
+        """Filter with fraction frequency."""
+        random_frequency = self.secret_rand.uniform(0, 1)
+        frequency = float(Fraction(phenotype_entry["frequency"]))
+        self.check_frequency_threshold(frequency, phenotype_entry, random_frequency)
+
+    def check_float_frequency(self, phenotype_entry: dict):
+        """Filter with float threshold."""
+        random_frequency = self.secret_rand.uniform(0, 1)
+        frequency = float((phenotype_entry["frequency"]))
+        self.check_frequency_threshold(frequency, phenotype_entry, random_frequency)
+
+    def check_frequency(self, phenotype_entry: dict):
+        """Filter for frequency."""
+        if str(phenotype_entry["frequency"]).startswith("HP:"):
+            self.check_hpo_frequency(phenotype_entry)
+        elif str(phenotype_entry["frequency"]).endswith("%"):
+            self.check_percentage_frequency(phenotype_entry)
+        elif "/" in str(phenotype_entry["frequency"]):
+            self.check_fraction_frequency(phenotype_entry)
+        elif is_float(phenotype_entry["frequency"]) is True:
+            self.check_float_frequency(phenotype_entry)
+
+    def filter_phenotype_entries(self, frequency_df: pl.DataFrame, max_number: int):
+        """Filter annotations based on frequency."""
+        while len(self.filtered_df) < max_number:
+            for phenotype_entry in frequency_df.rows(named=True):
+                if len(self.filtered_df) >= max_number:
+                    break
+                self.check_frequency(phenotype_entry)
+        return pl.from_dicts(self.filtered_df)
+
+    def get_patient_terms(self):
+        """Get patient terms, filtered on frequency thresholds."""
+        return self.filter_phenotype_entries(
+            self.shuffle_dataframe(self.add_frequency()), self.get_number_of_terms()
+        )
+
+    def get_number_of_terms_to_randomise(self, patient_terms: pl.DataFrame):
+        """Get number of terms to randomise from filtered frequency set."""
+        return self.secret_rand.randint(0, int(len(patient_terms)))
+
+    def get_number_of_steps_for_randomisation(self):
+        """Get the number of steps to take in range 1-5 for making a term more/less specific."""
+        return self.secret_rand.randint(1, 5)
+
+    def return_less_or_more_specific(self):
+        """Generate a float between 0-1."""
+        return self.secret_rand.random()
+
+    def subsample_patient_terms(self, patient_terms: pl.dataframe):
+        """Get a subsample of patient terms to make more/less specific."""
+        return patient_terms.sample(self.get_number_of_terms_to_randomise(patient_terms))
+
+    def get_children_of_term(self, phenotype_entry: dict, steps: int):
+        """Get a child term of a hpo id from the number of steps specified."""
+        term_id = phenotype_entry["hpo_id"]
+        for _i in range(steps):
+            descendants = self.ontology.descendants(phenotype_entry["hpo_id"])
+            descendant = self.secret_rand.choice(list(descendants))
+            term_id = descendant
+        phenotype_entry["hpo_id"] = term_id
+        return phenotype_entry
+
+    def get_parents_of_terms(self, phenotype_entry: dict, steps: int):
+        """Get a parent term of a hpo id from the number of steps specified."""
+        term_id = phenotype_entry["hpo_id"]
+        for _i in range(steps):
+            parents = self.ontology.hierarchical_parents(term_id)
+            parent = self.secret_rand.choice(parents)
+            rels = self.ontology.entity_alias_map(parent)
+            term = "".join(rels[(list(rels.keys())[0])])
+            if term.startswith("Abnormality of") or term_id == "HP:0000118":
+                break
+            else:
+                term_id = parent
+        phenotype_entry["hpo_id"] = term_id
+        return phenotype_entry
+
+    @staticmethod
+    def remove_terms_to_be_randomised(patient_terms: pl.DataFrame, subset: pl.DataFrame):
+        """Remove terms selected to be randomised from patient terms."""
+        subset_list = subset.to_dicts()
+        for phenotype_entry in subset_list:
+            patient_terms = patient_terms.filter(pl.col("hpo_id") != phenotype_entry["hpo_id"])
+        return patient_terms
+
+    def alter_term_specificity(self, new_phenotype_terms: [dict], phenotype_entry: dict):
+        """Alter the hpo id specificity - making less specific if the float is less than 0.5,
+        otherwise the term is made more specific."""
+        if self.return_less_or_more_specific() < 0.5:
+            new_phenotype_terms.append(
+                self.get_parents_of_terms(
+                    phenotype_entry, self.get_number_of_steps_for_randomisation()
+                )
+            )
+        else:
+            new_phenotype_terms.append(
+                self.get_children_of_term(
+                    phenotype_entry, self.get_number_of_steps_for_randomisation()
+                )
+            )
+
+    def patient_term_annotation_set(self):
+        """Get the final patient term annotation set."""
+        patient_terms = self.get_patient_terms()
+        if len(patient_terms) == 1:
+            return patient_terms
+        patient_terms_sub_sample = self.subsample_patient_terms(patient_terms)
+        new_phenotype_terms = []
+        for phenotype_entry in patient_terms_sub_sample.rows(named=True):
+            self.alter_term_specificity(new_phenotype_terms, phenotype_entry)
+        patient_terms_filtered = self.remove_terms_to_be_randomised(
+            patient_terms, patient_terms_sub_sample
+        )
+        final_patient_terms = patient_terms_filtered.to_dicts() + new_phenotype_terms
+        return pl.from_dicts(final_patient_terms)
+
+
 class PhenotypeAnnotationToPhenopacketConverter:
     def __init__(self, human_phenotype_ontology):
         self.human_phenotype_ontology = human_phenotype_ontology
@@ -162,6 +347,15 @@ class PhenotypeAnnotationToPhenopacketConverter:
         else:
             return None
 
+    def create_phenotypic_features(self, omim_disease_df: pl.DataFrame):
+        """Create a list of phenotypic features."""
+        phenotypic_features = []
+        for phenotype_annotation_entry in omim_disease_df.rows(named=True):
+            phenotypic_feature = self.create_phenotypic_feature(phenotype_annotation_entry)
+            if phenotypic_feature is not None:
+                phenotypic_features.append(phenotypic_feature)
+        return phenotypic_features
+
     @staticmethod
     def create_disease(phenotype_annotation_entry: dict) -> Disease:
         """Create a Disease object."""
@@ -213,13 +407,9 @@ class PhenotypeAnnotationToPhenopacketConverter:
     def create_phenopacket(
         self, omim_disease_df: pl.DataFrame, onset: OnsetTerm = None
     ) -> PhenopacketFile:
-        phenotypic_features = []
-        phenotype_annotation_entry = ""
-        for phenotype_annotation_entry in omim_disease_df.rows(named=True):
-            phenotype_annotation_entry = phenotype_annotation_entry
-            phenotypic_feature = self.create_phenotypic_feature(phenotype_annotation_entry)
-            if phenotypic_feature is not None:
-                phenotypic_features.append(phenotypic_feature)
+        """Create a Phenopacket object."""
+        phenotypic_features = self.create_phenotypic_features(omim_disease_df)
+        phenotype_annotation_entry = omim_disease_df.to_dicts()[0]
         return PhenopacketFile(
             phenopacket=Phenopacket(
                 id=phenotype_annotation_entry["disease_name"].lower().replace(" ", "_"),
@@ -239,6 +429,7 @@ class PhenopacketUtil:
         self.phenopacket = phenopacket
 
     def return_phenopacket_disease(self) -> [str]:
+        """Return the disease object from a phenopacket."""
         return self.phenopacket.diseases[0]
 
 
@@ -247,46 +438,10 @@ class PhenopacketInterpretationExtender:
         self.phenopacket = phenopacket
 
     @staticmethod
-    def add_variant_genomic_interpretation(variant_entry: dict, gene_identifier_updater):
-        try:
-            return GenomicInterpretation(
-                subject_or_biosample_id="patient1",
-                interpretation_status=0,
-                variant_interpretation=VariantInterpretation(
-                    acmg_pathogenicity_classification=variant_entry["ClinicalSignificance"],
-                    variation_descriptor=VariationDescriptor(
-                        id="clinvar:" + str(variant_entry["VariationID"]),
-                        gene_context=GeneDescriptor(
-                            value_id=gene_identifier_updater.find_identifier(
-                                variant_entry["GeneSymbol"]
-                            ),
-                            symbol=variant_entry["GeneSymbol"],
-                        ),
-                        vcf_record=VcfRecord(
-                            genome_assembly=variant_entry["Assembly"],
-                            chrom=variant_entry["Chromosome"],
-                            pos=variant_entry["Start"],
-                            ref=variant_entry["ReferenceAllele"]
-                            if variant_entry["ReferenceAllele"] != "na"
-                            else variant_entry["ReferenceAlleleVCF"],
-                            alt=variant_entry["AlternateAllele"]
-                            if variant_entry["AlternateAllele"] != "na"
-                            else variant_entry["AlternateAlleleVCF"],
-                        ),
-                    ),
-                ),
-            )
-        except TypeError:
-            print("N/A value", variant_entry)
-            return None
-        except KeyError:
-            print(f"Unable to find gene_symbol for {variant_entry['entrez_id']}")
-            return None
-
-    @staticmethod
-    def add_gene_genomic_interpretation(
+    def create_gene_genomic_interpretation(
         gene_to_phenotype_entry: dict, gene_identifier_updater: GeneIdentifierUpdater
     ):
+        """Create genomic interpretation for a gene-to-phenotype relationship."""
         try:
             gene_symbol = gene_identifier_updater.obtain_gene_symbol_from_identifier(
                 str(gene_to_phenotype_entry["entrez_id"])
@@ -308,42 +463,18 @@ class PhenopacketInterpretationExtender:
             print("N/A value", gene_to_phenotype_entry)
             return None
 
-    def create_variant_genomic_interpretations(
-        self, filtered_variant_summary: pl.DataFrame, gene_identifier_updater
-    ):
-        genomic_interpretations = []
-        for variant_entry in filtered_variant_summary.rows(named=True):
-            genomic_interpretation = self.add_variant_genomic_interpretation(
-                variant_entry, gene_identifier_updater
-            )
-            if genomic_interpretation is not None:
-                genomic_interpretations.append(genomic_interpretation)
-        return genomic_interpretations
-
     def create_gene_genomic_interpretations(
         self, omim_disease_phenotype_gene_map, gene_identifier_updater
     ):
+        """Create list of genomic interpretations for known gene-to-phenotype relationships."""
         genomic_interpretations = []
         for phenotype_entry in omim_disease_phenotype_gene_map.rows(named=True):
-            genomic_interpretation = self.add_gene_genomic_interpretation(
+            genomic_interpretation = self.create_gene_genomic_interpretation(
                 phenotype_entry, gene_identifier_updater
             )
             if genomic_interpretation is not None:
                 genomic_interpretations.append(genomic_interpretation)
         return genomic_interpretations
-
-    def create_variant_diagnosis(
-        self, filtered_variant_summary: pl.DataFrame, disease: Disease, gene_identifier_updater
-    ):
-        return Diagnosis(
-            disease=OntologyClass(
-                id=disease.term.id,
-                label=disease.term.label,
-            ),
-            genomic_interpretations=self.create_variant_genomic_interpretations(
-                filtered_variant_summary, gene_identifier_updater
-            ),
-        )
 
     def create_gene_diagnosis(
         self,
@@ -351,6 +482,7 @@ class PhenopacketInterpretationExtender:
         gene_identifier_updater,
         disease: Disease,
     ):
+        """Create a diagnosis object for known gene-to-phenotype relationships."""
         genomic_interpretations = self.create_gene_genomic_interpretations(
             omim_disease_phenotype_gene_map, gene_identifier_updater
         )
@@ -366,34 +498,8 @@ class PhenopacketInterpretationExtender:
             else None
         )
 
-    def create_variant_interpretation(self, filtered_variant_summary, gene_identifier_updater):
-        phenopacket_util = PhenopacketUtil(self.phenopacket)
-        disease = phenopacket_util.return_phenopacket_disease()
-        return Interpretation(
-            id=disease.term.label + "-interpretation",
-            progress_status=0,
-            diagnosis=self.create_variant_diagnosis(
-                filtered_variant_summary, disease, gene_identifier_updater
-            ),
-        )
-
-    @staticmethod
-    def add_clinvar_resource(phenopacket: Phenopacket):
-        phenopacket.meta_data.resources.extend(
-            [
-                Resource(
-                    id="clinvar",
-                    name="Clinical Variation",
-                    url="https://www.ncbi.nlm.nih.gov/clinvar/",
-                    version="2023-04-06",
-                    namespace_prefix="clinvar",
-                    iri_prefix="https://www.ncbi.nlm.nih.gov/clinvar/variation/",
-                )
-            ]
-        )
-        return phenopacket
-
     def create_gene_interpretation(self, omim_disease_phenotype_gene_map, gene_identifier_updater):
+        """Create an interpretation object for known gene-to-phenotype relationships."""
         phenopacket_util = PhenopacketUtil(self.phenopacket)
         disease = phenopacket_util.return_phenopacket_disease()
         diagnosis = self.create_gene_diagnosis(
@@ -409,28 +515,10 @@ class PhenopacketInterpretationExtender:
             else None
         )
 
-    def add_variant_interpretation_to_phenopacket(
-        self, filtered_variant_summary, gene_identifier_updater
-    ):
-        phenopacket_copy = copy(self.phenopacket)
-        interpretation = self.create_variant_interpretation(
-            filtered_variant_summary, gene_identifier_updater
-        )
-        if interpretation is not None:
-            phenopacket_copy.interpretations.extend(
-                [
-                    self.create_variant_interpretation(
-                        filtered_variant_summary, gene_identifier_updater
-                    )
-                ]
-            )
-            return self.add_clinvar_resource(phenopacket_copy)
-        else:
-            pass
-
     def add_gene_interpretation_to_phenopacket(
         self, omim_disease_phenotype_gene_map, gene_identifier_updater
     ):
+        """Add interpretations to phenopacket."""
         phenopacket_copy = copy(self.phenopacket)
         interpretations = [
             self.create_gene_interpretation(
@@ -440,3 +528,19 @@ class PhenopacketInterpretationExtender:
         if interpretations is not None:
             phenopacket_copy.interpretations.extend(interpretations)
             return phenopacket_copy
+
+
+@dataclass
+class FrequencyTerm:
+    lower: Union[int, str]
+    upper: Union[int, str]
+
+
+frequency_hpo = {
+    "HP:0040281": FrequencyTerm(0, 0),
+    "HP:0040282": FrequencyTerm(1, 4),
+    "HP:0040283": FrequencyTerm(5, 29),
+    "HP:0040284": FrequencyTerm(30, 79),
+    "HP:0040285": FrequencyTerm(80, 99),
+    "HP:0040286": FrequencyTerm(100, 100),
+}
