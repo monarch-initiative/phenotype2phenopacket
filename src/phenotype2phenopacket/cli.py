99433c425b7bc61d79f712ab893fb46586de6d1e
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-05 14:03:39 +0100

    removing add-variants commands

--- src/phenotype2phenopacket/cli.py
+++ src/phenotype2phenopacket/cli.py
@@ -1,7 +1,8 @@
 import click
 
-from phenotype2phenopacket.cli_add import add_genes_command, add_variants_command
+from phenotype2phenopacket.cli_add import add_genes_command
 from phenotype2phenopacket.cli_convert import convert_to_phenopackets_command
+from phenotype2phenopacket.cli_create import create_synthetic_patient_command
 
 
 @click.group()
@@ -10,7 +11,7 @@ def main():
 
 
 main.add_command(convert_to_phenopackets_command)
-main.add_command(add_variants_command)
 main.add_command(add_genes_command)
+main.add_command(create_synthetic_patient_command)
 if __name__ == "__main__":
     main()
