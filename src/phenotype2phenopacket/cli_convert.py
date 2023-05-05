bc64c877b3e28dbd5058a4a61956996b33a93ea2
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-05 15:56:13 +0100

    adding num_disease cli parameter

--- src/phenotype2phenopacket/cli_convert.py
+++ src/phenotype2phenopacket/cli_convert.py
@@ -15,6 +15,14 @@ from phenotype2phenopacket.utils.utils import read_phenotype_annotation_file
     type=Path,
 )
 @click.option(
+    "--num-disease",
+    "-n",
+    required=False,
+    help="Number of diseases to create synthetic patient phenopackets for.",
+    type=int,
+    default=0,
+)
+@click.option(
     "--output-dir",
     "-o",
     required=True,
@@ -23,7 +31,8 @@ from phenotype2phenopacket.utils.utils import read_phenotype_annotation_file
     default="phenopackets",
     show_default=True,
 )
-def convert_to_phenopackets_command(phenotype_annotation: Path, output_dir: Path):
+def convert_to_phenopackets_command(phenotype_annotation: Path, num_disease: int, output_dir: Path):
+    """Convert a phenotype annotation file (phenotype.hpoa) to a set of disease phenopackets."""
     output_dir.mkdir(exist_ok=True)
     phenotype_annotation_df = read_phenotype_annotation_file(phenotype_annotation)
-    convert_to_phenopackets(phenotype_annotation_df, output_dir)
+    convert_to_phenopackets(phenotype_annotation_df, num_disease, output_dir)
