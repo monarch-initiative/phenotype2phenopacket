356f21b766e19ebc4715bd8e08d8564fc5511949
Yasemin Bridges <y.bridges@qmul.ac.uk>
2023-05-05 14:04:09 +0100

    adding command for creating synthetic patients

---
+++ src/phenotype2phenopacket/cli_create.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import click
+
+from phenotype2phenopacket.create.create import create_synthetic_patient
+from phenotype2phenopacket.utils.utils import read_phenotype_annotation_file
+
+
+@click.command("create")
+@click.option(
+    "--phenotype-annotation",
+    "-p",
+    required=True,
+    help="Path to phenotype.hpoa.",
+    type=Path,
+)
+@click.option(
+    "--num-disease",
+    "-n",
+    required=False,
+    help="Number of diseases to create synthetic patient phenopackets for.",
+    type=int,
+    default=0
+)
+@click.option(
+    "--output-dir",
+    "-o",
+    required=True,
+    help="Path to output directory.",
+    type=Path,
+    default="phenopackets",
+    show_default=True,
+)
+def create_synthetic_patient_command(phenotype_annotation: Path, num_disease: int, output_dir: Path):
+    output_dir.mkdir(exist_ok=True)
+    phenotype_annotation_df = read_phenotype_annotation_file(phenotype_annotation)
+    create_synthetic_patient(phenotype_annotation_df, num_disease, output_dir)
