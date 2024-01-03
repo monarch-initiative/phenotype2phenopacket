676e587f0b026cdb628dc66365ff3383bda886a8
Yasemin Bridges <y.bridges@qmul.ac.uk>
2024-01-03 16:31:07 +0000

    add version to metadata for phenopacket from phenotype annotation

--- src/phenotype2phenopacket/create/create.py
+++ src/phenotype2phenopacket/create/create.py
@@ -8,11 +8,20 @@ from phenotype2phenopacket.utils.phenopacket_utils import (
     SyntheticPatientGenerator,
     write_phenopacket,
 )
-from phenotype2phenopacket.utils.utils import load_ontology, load_ontology_factory
+from phenotype2phenopacket.utils.utils import (
+    get_phenotype_annotation_version,
+    load_ontology,
+    load_ontology_factory,
+    read_phenotype_annotation_file,
+)
 
 
 def create_synthetic_patient_phenopacket(
-    human_phenotype_ontology, omim_disease: pl.DataFrame, ontology_factory, output_dir: Path
+    human_phenotype_ontology,
+    omim_disease: pl.DataFrame,
+    ontology_factory,
+    output_dir: Path,
+    hpoa_version: str,
 ):
     """Create a synthetic patient phenopacket from a set of phenotype entries for a specific OMIM disease."""
     synthetic_patient_generator = SyntheticPatientGenerator(
@@ -21,19 +30,19 @@ def create_synthetic_patient_phenopacket(
     patient_terms = synthetic_patient_generator.patient_term_annotation_set()
     phenopacket_file = PhenotypeAnnotationToPhenopacketConverter(
         human_phenotype_ontology
-    ).create_phenopacket(patient_terms, synthetic_patient_generator.get_onset_range())
+    ).create_phenopacket(patient_terms, hpoa_version, synthetic_patient_generator.get_onset_range())
     write_phenopacket(
         phenopacket_file.phenopacket, output_dir.joinpath(phenopacket_file.phenopacket_path)
     )
 
 
-def create_synthetic_patients(
-    phenotype_annotation: pl.DataFrame, num_disease: int, output_dir: Path
-):
+def create_synthetic_patients(phenotype_annotation: Path, num_disease: int, output_dir: Path):
     """Create a set of synthetic patient phenopackets from a phenotype annotation file."""
+    phenotype_annotation_df = read_phenotype_annotation_file(phenotype_annotation)
+    phenotype_annotation_version = get_phenotype_annotation_version(phenotype_annotation)
     human_phenotype_ontology = load_ontology()
     ontology_factory = load_ontology_factory()
-    omim_diseases = phenotype_annotation.filter(
+    omim_diseases = phenotype_annotation_df.filter(
         pl.col("database_id").str.starts_with("OMIM")
     ).filter(pl.col("aspect") == "P")
     grouped_omim_diseases = omim_diseases.partition_by(by="database_id", maintain_order=True)
@@ -41,5 +50,9 @@ def create_synthetic_patients(
         grouped_omim_diseases = random.sample(grouped_omim_diseases, num_disease)
     for omim_disease in grouped_omim_diseases:
         create_synthetic_patient_phenopacket(
-            human_phenotype_ontology, omim_disease, ontology_factory, output_dir
+            human_phenotype_ontology,
+            omim_disease,
+            ontology_factory,
+            output_dir,
+            phenotype_annotation_version,
         )
